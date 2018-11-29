46a33cecdb0c69996131acbf31d25519a0a70d9c
Muxima123 <raffael_thiel@hotmail.com>
2018-11-29 20:12:12 +0100

    gugus uskommentiert

--- eclipse-workspace/MajestyGUI_Merge/src/server/server_model.java
+++ eclipse-workspace/MajestyGUI_Merge/src/server/server_model.java
@@ -320,14 +320,14 @@ public class server_model {
 			}
 		}
 		
-		VisibilityMsg vismsg1 = new VisibilityMsg(erster, "false");
-		broadcast(vismsg1);	
-		logger.info("set zweiter visible false " + zweiter);
-		
-		VisibilityMsg vismsg2 = new VisibilityMsg(zweiter, "false");
-		broadcast(vismsg2);	
-		logger.info("set zweiter visible false " + zweiter);
-		
+//		VisibilityMsg vismsg1 = new VisibilityMsg(erster, "false");
+//		broadcast(vismsg1);	
+//		logger.info("set zweiter visible false " + zweiter);
+//		
+//		VisibilityMsg vismsg2 = new VisibilityMsg(zweiter, "false");
+//		broadcast(vismsg2);	
+//		logger.info("set zweiter visible false " + zweiter);
+//		
 		
 		System.out.println("all complete: " + allComplete);
 		
